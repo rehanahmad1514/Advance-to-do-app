ed08d746c1384d5c15cc977552db93b9a107fd95
rehanahmad1514 <rehan.ahmad1514@gmail.com>
2025-03-28 01:45:26 +0530

    some changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './redux/store.jsx'
-// import { Toaster } from "react-hot-to
+import store  from './redux/store.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
